d72439211b620aff281f09daa8f8d02ae9675d11
Conor Coker <conorcoker@gmail.com>
2024-10-10 14:24:37 +0100

    Mark task as done

--- conors_react_app/src/components/Task.js
+++ conors_react_app/src/components/Task.js
@@ -2,10 +2,11 @@ import react from 'react';
 
 const Task = (props) => {
     return (
-        <div className='card'>
+        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
             <p className='title'>{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className='description'>Description: {props.description}</p>
+            <button onClick={props.markDone} className='doneButton'>Done</button>
         </div>
     );
 };
