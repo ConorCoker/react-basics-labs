b79dc42115baed9fbc83457dabdf0ac53dd09d47
Conor Coker <conorcoker@gmail.com>
2024-10-10 16:28:03 +0100

    Delete a task

--- conors_react_app/src/App.js
+++ conors_react_app/src/App.js
@@ -16,6 +16,12 @@ function App() {
     setTaskState({tasks});
     console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex , 1)
+    setTaskState({tasks});
+    console.log(`${taskIndex} deleted`);
+  }
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -27,6 +33,7 @@ function App() {
       key={task.id}
       done={task.done}
       markDone={() => doneHandler(index)}
+      deleteTask = {() => deleteHandler(index)}
     />
   ))}
     </div>
